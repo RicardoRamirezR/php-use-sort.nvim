dd5bb275b841e1af049c88f9f3d81aee87db94ee
Ricardo Ramirez R <ricardo.ramirez.r@gmail.com>
2024-05-22 08:34:19 -0500

    refactor

--- lua/php-use-sort/properties.lua
+++ lua/php-use-sort/properties.lua
@@ -1,33 +1,32 @@
 local utils = require("php-use-sort.utils")
-
-local Properties = {}
-
 local ts = vim.treesitter
 local ts_utils = require("nvim-treesitter.ts_utils")
 
+local Properties = {}
+
 local function get_comments(property, range)
   local node = ts_utils.get_previous_node(property, false, false)
-  local table_lines = {}
+  local comments = {}
 
-  if node == nil or node:type() ~= "comment" then
-    return table_lines
+  if not node or node:type() ~= "comment" then
+    return comments
   end
 
   local start_row, _, end_row, _ = node:range()
   range.min = math.min(range.min, start_row + 1)
   range.max = math.max(range.max, end_row + 1)
 
-  local buff_lines = vim.api.nvim_buf_get_lines(0, start_row, end_row + 1, false)
-  for _, buf_line in ipairs(buff_lines) do
-    table.insert(table_lines, buf_line)
+  local lines = vim.api.nvim_buf_get_lines(0, start_row, end_row + 1, false)
+  for _, line in ipairs(lines) do
+    table.insert(comments, line)
   end
 
-  return table_lines
+  return comments
 end
 
 local function extract_properties_declarations(class_body)
-  local range = { min = math.huge, max = 0 }
   local lines = {}
+  local range = { min = math.huge, max = 0 }
 
   local property_query = [[
     (property_declaration
@@ -40,28 +39,34 @@ local function extract_properties_declarations(class_body)
         "const" @modifier
         (const_element) @property_element
     ) @prop
-    ]]
+  ]]
 
   local query = ts.query.parse("php", property_query)
 
   for _, match, _ in query:iter_matches(class_body, 0) do
-    local line = { "", "", "", "", "", "" }
+    local line = { vis = "", modifier = "", element = "", raw = {} }
     for id, node in pairs(match) do
-      line[id] = ts.get_node_text(node, 0)
-      if id == 2 and line[id] == "const" then
-        line[1] = line[1] .. " const"
+      local node_text = ts.get_node_text(node, 0)
+      if id == 1 then
+        line.vis = node_text
+      elseif id == 2 then
+        line.modifier = node_text
+        if node_text == "const" then
+          line.vis = line.vis .. " const"
+        end
+      elseif id == 3 then
+        line.element = node_text
       end
+
       if id == 4 then
         local start_row, _, end_row, _ = node:range()
         range.min = math.min(range.min, start_row + 1)
         range.max = math.max(range.max, end_row + 1)
-
-        local table_lines = get_comments(node, range)
-        local buff_lines = vim.api.nvim_buf_get_lines(0, start_row, end_row + 1, false)
-        for _, buf_line in ipairs(buff_lines) do
-          table.insert(table_lines, buf_line)
+        line.raw = get_comments(node, range)
+        local element_lines = vim.api.nvim_buf_get_lines(0, start_row, end_row + 1, false)
+        for _, element_line in ipairs(element_lines) do
+          table.insert(line.raw, element_line)
         end
-        line["raw"] = table_lines
       end
     end
     table.insert(lines, line)
@@ -71,86 +76,83 @@ local function extract_properties_declarations(class_body)
 end
 
 local function compare_properties(a, b)
-  -- Sort by statics first
-  if a[2] == "const" and b[2] ~= "const" then
+  -- Sort by constant first
+  if a.modifier == "const" and b.modifier ~= "const" then
     return true
-  elseif a[2] ~= "const" and b[2] == "const" then
+  elseif a.modifier ~= "const" and b.modifier == "const" then
     return false
   end
 
-  if a[2] == "static" and b[2] ~= "static" then
+  -- Sort by statics first
+  if a.modifier == "static" and b.modifier ~= "static" then
     return true
-  elseif a[2] ~= "static" and b[2] == "static" then
+  elseif a.modifier ~= "static" and b.modifier == "static" then
     return false
   end
 
   -- If both are static or both are not static, sort by access modifier
   local access_order = { ["private"] = 1, ["protected"] = 2, ["public"] = 3 }
-  if access_order[a[1]] ~= access_order[b[1]] then
-    return access_order[a[1]] < access_order[b[1]]
+  if access_order[a.vis] ~= access_order[b.vis] then
+    return access_order[a.vis] < access_order[b.vis]
   end
 
   -- If access modifiers are the same, sort by property name
-  return a[3] < b[3]
+  return a.element < b.element
 end
 
-local function sort_properties(lines, range, options)
-  if not options.enable then
-    return
-  end
+local function sort_properties(properties, range, options)
+  local original_properties = utils.tablecopy(properties)
+  table.sort(properties, compare_properties)
 
-  local copied_table = utils.tablecopy(lines)
-
-  table.sort(lines, compare_properties)
-
-  if not utils.table_changed(copied_table, lines) then
+  if not utils.table_changed(original_properties, properties) then
     return
   end
 
   if options.space == "between properties" then
-    local lines_with = {}
-    for _, line in ipairs(lines) do
-      table.insert(lines_with, { raw = line.raw })
-      table.insert(lines_with, { raw = { "" } })
+    local properties_with_spaces = {}
+    for _, line in ipairs(properties) do
+      table.insert(properties_with_spaces, { raw = line.raw })
+      table.insert(properties_with_spaces, { raw = { "" } })
     end
-    lines = lines_with
+    properties = properties_with_spaces
   end
 
   if options.space == "between types" then
-    local type = lines[1][1]
-    local lines_with = {}
-    for _, line in ipairs(lines) do
-      if type ~= line[1] then
-        type = line[1]
-        table.insert(lines_with, { raw = { "" } })
+    local previous_type = properties[1].modifier
+    local spaced_properties = {}
+    for _, property in ipairs(properties) do
+      if property.modifier ~= previous_type then
+        table.insert(spaced_properties, { raw = { "" } })
+        previous_type = property.modifier
       end
-      table.insert(lines_with, { raw = line.raw })
+      table.insert(spaced_properties, { raw = property.raw })
     end
-    lines = lines_with
+    properties = spaced_properties
   end
 
-  utils.update_buffer(range, lines)
+  utils.update_buffer(range, properties)
 end
 
 function Properties.sort(root, lang, options)
-  local query = ts.query.parse(lang, "((class_declaration) @class)")
-  local properties_info = {}
+  if not options.enable then
+    return
+  end
 
-  for _, node, _, _ in query:iter_captures(root, 0) do
+  local query = ts.query.parse(lang, "((class_declaration) @class)")
+  local class_properties = {}
+
+  for _, node, _ in query:iter_captures(root, 0) do
     if node:type() == "class_declaration" then
-      local class_lines, class_range = extract_properties_declarations(node)
-      if next(class_lines) then
-        table.insert(properties_info, { properties = class_lines, range = class_range })
+      local properties, range = extract_properties_declarations(node)
+      if next(properties) then
+        table.insert(class_properties, { properties = properties, range = range })
       end
     end
   end
 
-  for i = #properties_info, 1, -1 do
-    local class_info = properties_info[i]
-    local class_lines = class_info.properties
-    local class_range = class_info.range
-
-    sort_properties(class_lines, class_range, options)
+  for i = #class_properties, 1, -1 do
+    local class_info = class_properties[i]
+    sort_properties(class_info.properties, class_info.range, options)
   end
 end
 
